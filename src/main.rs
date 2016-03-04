2a25dfc481289b2883063a4fda270824cb6f569e
Jon Daniel <binarycleric@gmail.com>
2016-03-04 16:55:52 -0500

    Accepts piped in stdin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use std::env;
+use std::io;
+use std::io::Read;
+
+fn from_stdin() {
+    let mut buffer = String::new();
+
+    match io::stdin().read_to_string(&mut buffer) {
+        Err(error) => println!("error: {}", error),
+        _ => {
+            println!("{}", buffer);
+        }
+    }
+}
 
 fn main() {
     let input = env::args().nth(1);
@@ -7,6 +20,6 @@ fn main() {
         Some(value) => { 
             println!("{}", value); 
         },
-        None => { /* do nothing */ },
+        None => from_stdin()
     }
 }
